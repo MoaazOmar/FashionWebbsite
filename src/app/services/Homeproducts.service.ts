f9543d3bf81abd881f5225287672b199c81abc3e
MoaazOmar <133528519+MoaazOmar@users.noreply.github.com>
2025-04-02 21:56:52 +0200

    edited

--- src/app/services/Homeproducts.service.ts
+++ src/app/services/Homeproducts.service.ts
@@ -7,15 +7,14 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class HomeProductsService {
-  private baseUrl = 'http://localhost:3000'; 
+  private apiUrl = 'https://holy-althea-moaazomar-463f67fb.koyeb.app/';
 
   constructor(private _http: HttpClient) { }
-  private apiUrl = 'http://localhost:3000';
   getProducts(gender: string = '', limit: number = 3, skip: number = 0): Observable<Product[]> {
     let url = `${this.apiUrl}/?limit=${limit}&skip=${skip}`;
     if (gender) {
       url += `&gender=${gender}`;
     }
-    console.log('API URL:', url); // Debugging: Log the constructed URL
+    console.log('API URL:', url); 
     return this._http.get<Product[]>(url, { withCredentials: true });
   }}
